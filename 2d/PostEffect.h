3be43ea8fa37e3c02911a5290e01506e616a9103
IkeuchiYura <k020g1233@g.neec.ac.jp>
2022-04-13 13:05:53 +0900

    ポストエフェクト途中

--- 2d/PostEffect.h
+++ 2d/PostEffect.h
@@ -3,7 +3,22 @@
 
 class PostEffect : public Sprite
 {
+private:
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 public:
 	
-	
+	//コンストラクタ
+	PostEffect();
+
+	//初期化
+	void Initialize();
+
+	//描画
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private://メンバ変数
+	//テクスチャバッファ
+	ComPtr<ID3D12Resource> texBuff;
+	//SRV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap>descHeapSRV;
 };
