cc8dd66b2d7daa43828108c7ca83b412cbe225fa
IkeuchiYura <k020g1233@g.neec.ac.jp>
2022-06-26 15:40:52 +0900

    モデルを変更

--- 3d/ModelManager.cpp
+++ 3d/ModelManager.cpp
@@ -12,7 +12,7 @@ ModelManager* ModelManager::GetInstance() {
 void ModelManager::Initialize() {
 	/*LoadModel(Player, "cha_knight");
 	LoadModel(Enemy, "chr_sword");*/
-	LoadModel(Player, "Sphere");
+	LoadModel(Player, "player");
 	LoadModel(Skydome, "skydome");
 	LoadFBXModel(Pla, "Motti_move");
 	LoadFBXModel(Motti_moveF, "Motti_moveF");
